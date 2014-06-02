132b42b71c76ecbf6699649d5ff7b8433325a1c4
Daryl Halima <dhalima@gatech.edu>
2014-06-02 10:38:48 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Buy Daryl's Stuff </title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+</head>
+<body>
+
+	<div class="header">
+
+		<div class="wrapper">
+
+			<h1 class="branding-title"><a href="./"
