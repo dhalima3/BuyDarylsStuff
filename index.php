8607046147f7c0206c0f66ddc16c9c565e9c3904
Daryl Halima <dhalima@gatech.edu>
2014-06-18 10:10:34 -0400

    init
    init

--- index.php
+++ index.php
@@ -10,4 +10,21 @@
 
 		<div class="wrapper">
 
-			<h1 class="branding-title"><a href="./"
+			<h1 class="branding-title"><a href="./">Buy Daryl's Stuff</a></h1>
+
+			<ul class="nav">
+				<li class="shirts"><a href="#">Items</a></li>
+				<li class="contact"><a href="#">Contact</a></li>
+				<li class="cart"><a href="#">Shopping Cart</a></li>
+			</ul>
+
+		</div>
+	</div>
+
+	<div id="content">
+
+		<div class="section banner">
+
+			<div class="wrapper">
+
+				<div class="button">
