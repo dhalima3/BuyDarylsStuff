446f4bf4480069662c9f36ea3d105974994775b2
Daryl Halima <dhalima@gatech.edu>
2014-06-18 15:26:59 -0400

    added items

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <!--Buttons.js-->
+    <script type="text/javascript" src="js/buttons.js"></script>
+
+   	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <div id="footer">
+    	<div class="container">
+			<p id="footer-date">&copy;<?php echo date('Y'); ?> Daryl </p>
+    	</div>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 $pageTitle = "Random stuff sold by Daryl";
 $section = "home";
 include('inc/header.php'); ?>
-<html>
-Hi
-</html>	
+
+      <div class="section-banner">
+      	<div class="container">
+	      	<!-- <a href="#" class="button button-3d-highlight button-circle">Click here!</a> -->
+	    </div>  	
+      </div>
+
+      <div class="latest-items">
+      	
+      	<div class="container">
+
+			<h2>Daryl&rsquo;s Latest Items</h2>
+      	
+	      	<?php include("inc/items.php"); ?>
+	      	<!--First row of items -->
+	      	<div class="row">
+
+	      		<?php
+	      			$count_items = count($items);
+	      			$position = 0;
+	      			$list_view_html = "";
+	      			foreach($items as $item_id => $item){
+	      				$position++;
+	      				if ($count_items - $position < 8){
+	      					$list_view_html =get_list_view_html($item_id, $item). $list_view_html;
+	      				}
+	      			}
+	      			echo $list_view_html;
+	      		?>
+				    		
+			</div>
+		</div>
+	</div>
+
+<?php
+include('inc/footer.php') ?>
