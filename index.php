a365369c39ed764b25673f8418dbc4b55de169bf
Daryl Halima <dhalima@gatech.edu>
2014-06-18 13:02:31 -0400

    ui improvements

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
+<?php
+$pageTitle = "Random stuff sold by Daryl";
+$section = "home";
+include('inc/header.php'); ?>
 <html>
-<head>
-	<title>Buy Daryl's Stuff </title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Buy Daryl's Stuff</a></h1>
-
-			<ul class="nav">
-				<li class="shirts"><a href="#">Items</a></li>
-				<li class="contact"><a href="#">Contact</a></li>
-				<li class="cart"><a href="#">Shopping Cart</a></li>
-			</ul>
-
-		</div>
-	</div>
-
-	<div id="content">
-
-		<div class="section banner">
-
-			<div class="wrapper">
-
-				<div class="button">
+Hi
+</html>	
